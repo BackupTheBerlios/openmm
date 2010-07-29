602fbf363f036812f4b8a378ee4ec93c3dade72a
Jörg Bakker <jb@open-multimedia.org>
2010-07-29 22:31:31 +0200

    CHG dependency detection improvements in build system
    Basic libs, executables, and plugins should build now only if dependencies are found.
    Libraries are searched in a staging dir, where the compiled output is installed, too.

--- make.sh
+++ make.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-export CMAKE_LIBRARY_PATH=/Users/jb/devel/darwin/lib/
-export CMAKE_INCLUDE_PATH=/Users/jb/devel/darwin/include/
+export CMAKE_PREFIX_PATH=/Users/jb/devel/darwin
+#export CMAKE_PREFIX_PATH=/Users/jb/devel/iphone
 
 SRCDIR=${PWD}
 BINDIR=../ommbin
-#CMAKE_CMD="cmake -DCMAKE_INSTALL_PREFIX=/usr"
-CMAKE_CMD="cmake"
+#CMAKE_CMD="cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_PREFIX_PATH} -DCMAKE_TOOLCHAIN_FILE=toolchains/iphone"
+CMAKE_CMD="cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_PREFIX_PATH}"
+#CMAKE_CMD="cmake"
 
 if [ ! -d $BINDIR ]
 then
     mkdir -p $BINDIR
 fi
 
-if [ "$1" = "install" ]
-then
-    sudo make install
-elif [ "$1" = "distclean" ]
+#if [ "$1" = "install" ]
+#then
+#    sudo make install
+#elif [ "$1" = "distclean" ]
+if [ "$1" = "distclean" ]
 then
     rm -rf ${BINDIR}
-elif [ "$1" = "configure" ]
+elif [ "$1" = "config" ]
 then
     cd $BINDIR
     ${CMAKE_CMD} ${SRCDIR}
