b0694c0bfbe4d462b3b1889f2df572f0b991e375
Jörg Bakker <jb@open-multimedia.org>
2010-08-02 20:38:35 +0200

    ADD cross compile support.
    Added platform specific build configurations for iphone and iphone-simulator.

--- make.sh
+++ make.sh
@@ -1,31 +1,67 @@
 #!/bin/sh
 
-export CMAKE_PREFIX_PATH=/Users/jb/devel/darwin
-#export CMAKE_PREFIX_PATH=/Users/jb/devel/iphone
-
-SRCDIR=${PWD}
+VERBOSE=
+CMAKE_PREFIX_PATH=
+TOOLCHAIN=
 BINDIR=../ommbin
-#CMAKE_CMD="cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_PREFIX_PATH} -DCMAKE_TOOLCHAIN_FILE=toolchains/iphone"
-CMAKE_CMD="cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_PREFIX_PATH}"
-#CMAKE_CMD="cmake"
+CMAKE_CMD=cmake
+CMAKE_OPTS="-DCMAKE_BUILD_TYPE=None"
 
-if [ ! -d $BINDIR ]
+usage()
+{
+    echo >&2 \
+    "usage: $0 [-h] [-v] [-s stagingdir] [-t toolchain] [target]"
+    exit 1
+}
+
+while getopts hvs:t: opt
+do
+    case "$opt" in
+	h) usage;;
+        v) VERBOSE="VERBOSE=1";;
+        s) CMAKE_PREFIX_PATH="$OPTARG";;
+	t) TOOLCHAIN="$OPTARG";;
+        \?) usage;;    # unknown flag
+    esac
+done
+shift `expr $OPTIND - 1`
+
+if [ $# -gt 1 ]
 then
-    mkdir -p $BINDIR
+    usage
 fi
 
-#if [ "$1" = "install" ]
-#then
-#    sudo make install
-#elif [ "$1" = "distclean" ]
+export CMAKE_PREFIX_PATH
+echo "staging dir:" ${CMAKE_PREFIX_PATH}
+
+CMAKE_OPTS="${CMAKE_OPTS} -DCMAKE_INSTALL_PREFIX=${CMAKE_PREFIX_PATH}"
+SRCDIR=${PWD}
+
+# do a cross compile
+if [ ${TOOLCHAIN} ]
+then
+    echo "building with toolchain configuration" platform/${TOOLCHAIN}
+    BINDIR=${BINDIR}.${TOOLCHAIN}
+    CMAKE_OPTS="${CMAKE_OPTS} -DCMAKE_TOOLCHAIN_FILE=${SRCDIR}/platform/${TOOLCHAIN}"
+fi
+
+echo "build directory:" ${BINDIR}
+echo "cmake options:" ${CMAKE_OPTS}
+
+# run cmake in out of source build tree
+if [ ! -d ${BINDIR} ]
+then
+    mkdir -p ${BINDIR}
+fi
+
 if [ "$1" = "distclean" ]
 then
     rm -rf ${BINDIR}
 elif [ "$1" = "config" ]
 then
-    cd $BINDIR
-    ${CMAKE_CMD} ${SRCDIR}
+    cd ${BINDIR}
+    ${CMAKE_CMD} ${CMAKE_OPTS} ${SRCDIR}
 else
-    cd $BINDIR
-    make $1
+    cd ${BINDIR}
+    make ${VERBOSE} $1
 fi
