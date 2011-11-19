78dd4bacbc1a6b189505b00d3fa8d18a520cb076
Jörg Bakker <jb@open-multimedia.org>
2011-11-19 11:26:59 +0100

    ADD create package for windows.

--- cmake/ConfigurePackager.cmake
+++ cmake/ConfigurePackager.cmake
@@ -1,13 +1,26 @@
-#include(InstallRequiredSystemLibraries)
+if(LINUX)
 set(CPACK_GENERATOR
 "TGZ;STGZ;DEB"
 )
+elseif(WINDOWS)
+set(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS
+)
+set(CMAKE_INSTALL_DEBUG_LIBRARIES
+)
+include(InstallRequiredSystemLibraries)
+set(CPACK_GENERATOR
+"TGZ;NSIS"
+)
+endif(LINUX)
+
 set(CPACK_INSTALL_CMAKE_PROJECTS
 "${CMAKE_CURRENT_BINARY_DIR};omm;ALL;/"
 )
+
 set(CPACK_PACKAGE_CONTACT
 "jb@open-multimedia.org"
 )
+
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
 "open multimedia applications and libraries"
 )
@@ -16,9 +29,11 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
 #set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE
 #"amd64"
 #)
+
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
 "libc6 (>= 2.13), libgcc1 (>= 1:4.5.2)"
 )
+
 set(CPACK_DEBIAN_PACKAGE_SECTION
 "video"
 )
