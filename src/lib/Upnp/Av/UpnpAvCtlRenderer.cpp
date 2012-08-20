f1d3cd789090952db2d4bace3d7b219fcec0a407
Jörg Bakker <jb@open-multimedia.org>
2012-08-20 19:28:29 +0200

    FIX selecting single items in playlist to play them.

--- src/lib/Upnp/Av/UpnpAvCtlRenderer.cpp
+++ src/lib/Upnp/Av/UpnpAvCtlRenderer.cpp
@@ -350,6 +350,7 @@ MediaRendererGroupDelegate::mediaItemSelectedHandler2(MediaObjectSelectedNotific
     LOG(upnpav, debug, "media renderer delegate got media item notification: " + pMediaItemNotification->_pObject->getTitle());
     CtlMediaRenderer* pRenderer = static_cast<CtlMediaRenderer*>(_pDeviceGroup->getSelectedDevice());
     if (pRenderer) {
+        pRenderer->stopPressed();
         pRenderer->setObject2(pMediaItemNotification->_pObject, pMediaItemNotification->_pParentObject, pMediaItemNotification->_row);
         pRenderer->playPressed();
     }
